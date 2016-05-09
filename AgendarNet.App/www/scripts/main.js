f1950cc394fba36fe06bac6d7e56a3dd017970a9
Edgar Alexsandro de Mesquita <edgar.rj@live.com>
2016-05-09 09:15:55 -0300

    Implementação do serviço de autenticação Json Api do Wordpress

--- AgendarNet.App/www/scripts/main.js
+++ AgendarNet.App/www/scripts/main.js
@@ -1,4 +1,4 @@
-﻿angular.module('ionicApp', ['ionic'])
+﻿angular.module('ionicApp', ['ionic', 'ngCookies'])
 
 .config(function ($stateProvider, $urlRouterProvider) {
 
@@ -71,12 +71,114 @@
     $urlRouterProvider.otherwise('/sign-in');
 
 })
+    .service('sessionService', ['$cookieStore', function ($cookieStore) {
+        var localStoreAvailable = typeof (Storage) !== "undefined";
+        this.store = function (name, details) {
+            if (localStoreAvailable) {
+                if (angular.isUndefined(details)) {
+                    details = null;
+                } else if (angular.isObject(details) || angular.isArray(details) || angular.isNumber(+details || details)) {
+                    details = angular.toJson(details);
+                };
+                sessionStorage.setItem(name, details);
+            } else {
+                $cookieStore.put(name, details);
+            };
+        };
 
-.controller('SignInCtrl', function ($scope, $state) {
+        this.persist = function (name, details) {
+            if (localStoreAvailable) {
+                if (angular.isUndefined(details)) {
+                    details = null;
+                } else if (angular.isObject(details) || angular.isArray(details) || angular.isNumber(+details || details)) {
+                    details = angular.toJson(details);
+                };
+                localStorage.setItem(name, details);
+            } else {
+                $cookieStore.put(name, details);
+            }
+        };
+
+        this.get = function (name) {
+            if (localStoreAvailable) {
+                return getItem(name);
+            } else {
+                return $cookieStore.get(name);
+            }
+        };
+
+        this.destroy = function (name) {
+            if (localStoreAvailable) {
+                localStorage.removeItem(name);
+                sessionStorage.removeItem(name);
+            } else {
+                $cookieStore.remove(name);
+            };
+        };
+
+        var getItem = function (name) {
+            var data;
+            var localData = localStorage.getItem(name);
+            var sessionData = sessionStorage.getItem(name);
+
+            if (sessionData) {
+                data = sessionData;
+            } else if (localData) {
+                data = localData;
+            } else {
+                return null;
+            }
+
+            if (data === '[object Object]') { return null; };
+            if (!data.length || data === 'null') { return null; };
+
+            if (data.charAt(0) === "{" || data.charAt(0) === "[" || angular.isNumber(data)) {
+                return angular.fromJson(data);
+            };
+
+            return data;
+        };
+
+        return this;
+    }])
+.controller('SignInCtrl', function ($scope, $state, $http) {
+
+    var baseUri = "http://agendar.azurewebsites.net/api/";
+    var nonceUri = baseUri + "get_nonce/?controller=user&method=generate_auth_cookie";
 
     $scope.signIn = function (user) {
-        console.log('Sign-In', user);
-        $state.go('tabs.home');
+        var url = baseUri + "user/generate_auth_cookie/?insecure=cool";
+        
+
+        $http({
+            method: 'GET',
+            url: nonceUri
+        }).then(function loginSuccess(response) {
+
+            console.log(response);
+
+            var data = user;
+            data.nonce = response.data.nonce;
+
+            $http({
+                method: 'GET',
+                url: url,
+                params: data
+            }).then(function loginSuccess(response) {
+
+                $state.go('tabs.home');
+                console.log('Sign-In', user);
+
+            }, function loginError(response) {
+
+                console.log(response.data.error);
+            });
+
+        }, function loginError(response) {
+            
+        });
+
+        
     };
 
 })
